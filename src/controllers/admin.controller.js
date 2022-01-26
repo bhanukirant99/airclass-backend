4ca3db8968cc33b6265260c5f18b6f2ff98beb8f
Bhanu Kiran <bhanukirant99@gmail.com>
2022-01-26 15:06:58 +0530

    make it better

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -2,9 +2,9 @@ const httpStatus = require('http-status');
 const pick = require('../utils/pick');
 const ApiError = require('../utils/ApiError');
 const catchAsync = require('../utils/catchAsync');
-const Admin = require('../models');
-const Content = require('../models');
-const Course = require('../models');
+const {Admin} = require('../models');
+const {Content} = require('../models');
+const {Course} = require('../models');
 const { authService, userService, tokenService, emailService } = require('../services');
 
 exports.get_newCourse_page = (req, res) => {
