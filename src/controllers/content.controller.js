f1a4e2361afbff9eafd3291fe780e5db0abde8b7
Bhanu Kiran <bhanukirant99@gmail.com>
2022-02-04 22:48:23 +0530

    fix: content controller

--- src/controllers/content.controller.js
+++ src/controllers/content.controller.js
@@ -34,10 +34,10 @@ exports.get_all_course_contents = async(req, res) => {
 exports.create_newContent = async(req, res) => {
     const courseID = req.params.courseID
     newContent = await new Content({
-            classTitle: req.body.title,
+            classTitle: req.body.classTitle,
             courseID: courseID,
-            classVideo: req.body.videoUrl,
-            classDuration: req.body.watchHours,
+            classVideo: req.body.classVideo,
+            classDuration: req.body.classDuration,
         })
         // const course = await Course.find({ courseID });
         // console.log(course)
